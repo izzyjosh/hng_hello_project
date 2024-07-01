055526496b174614944d955decfd277289253e51
izzyjosh <calebayuba222@gmail.com>
2024-07-01 16:07:41 +0100

    Completed the task about to start deployment

---
+++ core/settings.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "djangecb3ceb36ead0450db47bfef46586a9cd77c02befd566d6af317493a6693a83bb583989fdd118c7657257c4a29f4b1fcadc8"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "main.apps.MainConfig",
+    "rest_framework"
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Password validation
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "static/"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#ipinfo API key
+IPINFO_API_TOKEN = "c5d3ea64ad4c30"
+
+#openwhethermap api key
+OPENWHETHER_API = "fc4b24a0bfb27829b9d480e3f8dc0c30"
---
+++ main/views.py
@@ -0,0 +1,55 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
+from django.conf import settings
+from rest_framework import status
+
+def get_city(ip_address):
+    ipinfo_token = settings.IPINFO_API_TOKEN
+    url = f"https://ipinfo.io/{ip_address}?token={ipinfo_token}"
+    try:
+        response = requests.get(url)
+        print(response.status_code)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("city")
+        else:
+            print("No")
+            return None
+    except requests.RequestException:
+        print("request failed")
+        return none
+
+def get_temperature(city):
+    openwhether_api_key = settings.OPENWHETHER_API
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={openwhether_api_key}&units=metric"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('main', {}).get('temp')
+        else:
+            return None
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+
+class Hello(APIView):
+
+    def get(self,request,format=None):
+        visitor_name = request.GET.get("visitor_name")
+        ipaddr = request.META.get("REMOTE_ADDR")
+        print(ipaddr)
+        city = get_city(ipaddr)
+        print(city)
+        temperature = get_temperature(city)
+        print(temperature)
+
+        response = {
+                "client_ip":f"{ipaddr}",
+                "location": f"{city}",
+                "greeting":f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {city}",
+                }
+        return Response(response,status=status.HTTP_200_OK)
+
