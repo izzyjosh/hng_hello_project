87cd1e58be9ab5c7a9712f171dc01c75190bbce2
izzyjosh <calebayuba222@gmail.com>
2024-07-05 22:27:47 +0100

    updates

---
+++ build_file.sh
@@ -0,0 +1,5 @@
+# build_files.sh
+
+pip install -r requirements.txt
+python3.x manage.py collectstatic --no-input --clear
+
--- core/settings.py
+++ core/settings.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
-    "rest_framework"
+    "rest_framework",
+
 ]
 
 MIDDLEWARE = [
